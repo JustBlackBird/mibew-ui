1eedfbf19d7abf88a786f4523b403bca5f90cf3a
Dmitriy Simushev <simushevds@gmail.com>
2016-12-25 21:24:17 +0300

    Use redux-thunk in OperatorApp

--- app/operator_app.jsx
+++ app/operator_app.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {connect, Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import OperatorApp from './operator/application.jsx';
 import operatorReducers from './operator/reducers';
 
 let reducers = combineReducers({operator: operatorReducers});
 
 export function run(initialState, rootElement) {
-    let store = createStore(reducers, initialState);
+    let store = createStore(reducers, initialState, applyMiddleware(thunk));
     let ConnectedOperatorApp = connect(state => state.operator)(OperatorApp);
 
     ReactDOM.render(
