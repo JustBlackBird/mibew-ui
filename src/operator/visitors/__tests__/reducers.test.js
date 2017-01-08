ba4db4a94b3bd2f1d51be6ade3b429594a34bbc7
Dmitriy Simushev <simushevds@gmail.com>
2017-01-08 21:01:57 +0300

    Improve test description for reducers tests

--- src/operator/visitors/__tests__/reducers.test.js
+++ src/operator/visitors/__tests__/reducers.test.js
@@ -2,14 +2,16 @@ import {expect} from 'chai';
 import {updateVisitors} from '../actions/';
 import reduceState from '../reducers';
 
-describe('UPDATE_VISITORS', () => {
-    it('should replace visitors with set from action', () => {
-        const visitors = [{foo: 'bar', baz: 'quux'}];
-        const initialState = [];
+describe('Visitors Reducers', () => {
+    describe('UPDATE_VISITORS', () => {
+        it('should replace visitors with set from action', () => {
+            const visitors = [{foo: 'bar', baz: 'quux'}];
+            const initialState = [];
 
-        const newState = reduceState(initialState, updateVisitors(visitors));
+            const newState = reduceState(initialState, updateVisitors(visitors));
 
-        // The state is just visitors list.
-        expect(newState).to.deep.equal(visitors);
+            // The state is just visitors list.
+            expect(newState).to.deep.equal(visitors);
+        });
     });
 });
