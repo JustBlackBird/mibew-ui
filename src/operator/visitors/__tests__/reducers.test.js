edcf61e9415637581bd1e2a05193ccba8c797b9a
Dmitriy Simushev <simushevds@gmail.com>
2017-01-08 20:51:25 +0300

    Add tests for reducers

---
+++ src/operator/visitors/__tests__/reducers.test.js
@@ -0,0 +1,15 @@
+import {expect} from 'chai';
+import {updateVisitors} from '../actions/';
+import reduceState from '../reducers';
+
+describe('UPDATE_VISITORS', () => {
+    it('should replace visitors with set from action', () => {
+        const visitors = [{foo: 'bar', baz: 'quux'}];
+        const initialState = [];
+
+        const newState = reduceState(initialState, updateVisitors(visitors));
+
+        // The state is just visitors list.
+        expect(newState).to.deep.equal(visitors);
+    });
+});
