47ea62a7c997193f05ed4b6770fb0ae8c505b46e
Dmitriy Simushev <simushevds@gmail.com>
2017-01-14 19:14:33 +0300

    Wrap the app with Bootstrap grid

--- src/operator/application.jsx
+++ src/operator/application.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Visitors from './visitors';
 import * as actions from './actions';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 export default class Application extends React.Component {
     handleInvite(visitorId) {
@@ -9,15 +10,19 @@ export default class Application extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Operator's home</h1>
-                <p>This is an operator's application!</p>
-                <hr />
-                <Visitors
-                    visitors={this.props.visitors}
-                    onInvite={this.handleInvite.bind(this)}
-                />
-            </div>
+            <Grid>
+                <Row>
+                    <Col md={8} mdOffset={2}>
+                        <h1>Operator's home</h1>
+                        <p>This is an operator's application!</p>
+                        <hr />
+                        <Visitors
+                            visitors={this.props.visitors}
+                            onInvite={this.handleInvite.bind(this)}
+                        />
+                    </Col>
+                </Row>
+            </Grid>
         );
     }
 };
